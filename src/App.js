c693017f295e4c3b7e6bd3b1c58bd1f007717ff7
Abdelrahman El-Adawy <adawy@oroboro.studio>
2020-04-12 22:16:38 +0200

    read raw data from csv

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import './App.css';
+
+//Libraries
+import { csv } from 'd3';
+
+//Components
 import { Pane } from 'evergreen-ui';
 import DropDownMenu from './components/dropdownmenu';
 import Slider from './components/customSlider';
@@ -9,17 +14,42 @@ import RGLMap from './components/Map';
 import data from './disposables/data.json';
 
 function App() {
-  return (
-    <div>
-      <DropDownMenu />
-      <Slider />
-      <Pane style={{ width: 1300, height: 400 }}>
-        <Chart data={data} />
-      </Pane>
-      <Radio />
-      <RGLMap />
-    </div>
-  );
+  const [csvData, setcsvData] = React.useState(null);
+
+  const TurnCSVintoSmthUseful = () => {
+    let rawData = csv('time_series_covid19_deaths_global.csv').then((data) => {
+      setcsvData(data);
+      return JsonFormatter(data);
+    });
+    return rawData;
+  };
+
+  React.useEffect(() => {
+    TurnCSVintoSmthUseful();
+  }, []);
+
+  return <div>{console.log(csvData)}</div>;
 }
 
 export default App;
+
+//parse normal data
+const JsonFormatter = (data) => {
+  const result = data.map((obj) => {
+    let newObj = {};
+    newObj.id = obj.Place;
+    newObj.Lat = obj.Lat;
+    newObj.Long = obj.Long;
+    newObj.data = [];
+    Object.keys(obj).forEach((key) => {
+      if (
+        /^(0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])[\/\-]\d{2}$/.test(key)
+      ) {
+        // if it matches the date format
+        newObj.data.push({ x: key, y: obj[key] });
+      }
+    });
+    return newObj;
+  });
+  return result;
+};
