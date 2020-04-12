3aef6fcaae3415c0897968edf25445fdaef1f0f0
Abdelrahman El-Adawy <adawy@oroboro.studio>
2020-04-12 22:07:22 +0200

    adding basic chart

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
+import { Pane } from 'evergreen-ui';
 import DropDownMenu from './components/dropdownmenu';
 import Slider from './components/customSlider';
+import Chart from './components/customChart';
+import data from './disposables/data.json';
 
 function App() {
   return (
     <div>
       <DropDownMenu />
       <Slider />
+      <Pane style={{ width: 1300, height: 400 }}>
+        <Chart data={data} />
+      </Pane>
     </div>
   );
 }
